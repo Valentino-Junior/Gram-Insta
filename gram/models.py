67aad6e6aca0ad1df693a5b2681bd517bfb4bb13
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-12-02 18:43:53 +0300

    Add Image class and registers it admin file

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Create your models here
+class Images(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=50)
+    caption = models.TextField(max_length=2000)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.image
