a7d97df784ff92fdbfd86b131cbcc162bdf37ba5
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-12-03 09:42:20 +0300

    Creates authentication

--- gram/views.py
+++ gram/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Images
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
     image = Images.objects.all().order_by('-id')
     return render(request, 'all-glam/home.html',{'image':image})
--- instagram/urls.py
+++ instagram/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url,include
+from django.contrib.auth import views 
+
 
 
 urlpatterns = [
-    url('^admin/', admin.site.urls),
-    url(r'',include('gram.urls'))
-
+    url('admin/', admin.site.urls),
+    url(r'',include('gram.urls')),
+    url(r'^register/$)',views.register,{"next_page":'login/'}),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
