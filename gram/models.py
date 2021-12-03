7cdf5092aca9bc55172c0f50c6a357ad4f70c572
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-12-03 22:29:52 +0300

    Add profile,like and comment class

--- gram/models.py
+++ gram/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.db.models.fields import related
+from django.contrib.auth.models import User
+
 
 # Create your models here
 class Images(models.Model):
@@ -7,6 +10,75 @@ class Images(models.Model):
     name = models.CharField(max_length=50)
     caption = models.TextField(max_length=2000)
     pub_date = models.DateTimeField(auto_now_add=True,null=True)
-
+    likes_count = models.IntegerField(default=0)
+    comm_count = models.IntegerField(default=0)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.name
+
+    def update_caption(self, new_caption):
+        self.image_caption = new_caption
+        self.save()
+
+    @classmethod
+    def get_images_by_user(cls, user):
+        images = cls.objects.filter(user=user)
+        return images
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def search_by_image_name(cls, search_term):
+        images = cls.objects.filter(
+            image_name__icontains=search_term)
+        return images
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    prof_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=1000, blank=True, null=True)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return self.phone_number
+
+    def save_profile(self):
+        self.save()
+
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_by_user(cls, user):
+        profile = cls.objects.filter(user=user)
+        return profile
+
+class Likes(models.Model):
+    image = models.ForeignKey(Images, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.id}:{self.image.id}'
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Images, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=50)
+    comm_date = models.DateTimeField(auto_now_add=True)
+
+    def save_comment(self):
+        self.save()
+    
+    def delete_comment(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.user.id}:{self.image.id}'
+
